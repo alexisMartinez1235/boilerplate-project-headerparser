298a8f024f0ca26c4eb8b9668d06f3bbaf74c55e
Alexis Martinez <67340366+alexisMartinez1235@users.noreply.github.com>
2022-08-01 22:02:57 +0000

    i add api/whoami route

---
+++ api/router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router()
+
+
+// your first API endpoint...
+router.get('/hello', (req, res) => {
+  res.json({ greeting: 'hello API' });
+});
+
+router.get('/whoami', (req, res) => {
+  const ipaddress = req.headers['x-forwarded-for'];
+  const software = req.headers["user-agent"];
+
+  console.log(software);
+  
+  res.json({
+    ipaddress,
+    language: req.acceptsLanguages(),
+    software
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const router = require('./api/router');
+
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -19,10 +21,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.use('/api', router);
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
